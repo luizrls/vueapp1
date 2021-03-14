a858ca54e2dc7d7c1d69768ca5800d9edb9e2785
Luis Rogerio <luis.lima.santos@everis.com>
2021-03-14 13:52:17 -0300

    no message

---
+++ src/components/feedback/Comments.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="containter">
+        <div class="Form">
+            <input type="text" name="Autor" />
+            <textarea name="Comentario" id="" cols="30" rows="10"></textarea>
+        </div>
+        <div class="History">
+
+        </div>
+    </div> container
+    
+</template>
+
+<script>
+export default {
+  name: 'Comments',
+  props: {
+    msg: String
+  }
+}
+</script>
